3bc4a18df8d6ef41fe6478c1f6b1ceb4b80ce6c8
ImDarkly <66973381+ImDarkly@users.noreply.github.com>
2021-10-19 12:00:37 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,16 @@ function openBurger() {
     var element = document.getElementById("burger");
     element.classList.toggle("active");
   }
+  
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 0) {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+});
+
+function hyperlinkFunction() {
+  document.querySelector('#menu').scrollIntoView({
+    behavior: "smooth"
+  })};
