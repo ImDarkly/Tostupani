ecc9d076ec8336c94f5b91ee22d0f21dc9c03e11
ImDarkly <66973381+ImDarkly@users.noreply.github.com>
2021-10-23 21:47:36 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 function openBurger() {
     var element = document.getElementById("burger");
     element.classList.toggle("active");
+    var element = document.getElementById("burger-content");
+    element.classList.toggle("show");
+    element.classList.toggle("hide");
+    var element = document.getElementById("navbar");
+    element.classList.toggle("hide");
   }
   
 $(window).scroll(function() {
